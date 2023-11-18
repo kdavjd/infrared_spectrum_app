e3ef2709cdf4c0d3810396f096b3879d32ed0087
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-11-18 16:16:42 +0300

    integral_action_callbacks extracted

--- UI/GraphicalArea/graphical_area.py
+++ UI/GraphicalArea/graphical_area.py
@@ -4,83 +4,59 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import scienceplots
 import pandas as pd
-from .custom_toolbar import CustomToolbar 
+from .custom_toolbar import CustomToolbar
+from .integral_action_callbacks import IntegralActionCallbacks
+from logger_config import logger
 plt.style.use(['science', 'no-latex', 'nature', 'grid'])
-       
+
 
 class GraphicalArea(QWidget):
     
     # Определение сигналов
     mouse_released_signal = pyqtSignal(tuple)
     
-    def __init__(self, parent=None):
-        super().__init__(parent)        
+    def __init__(self, graphical_area=None):
+        super().__init__(graphical_area)        
         # Создание фигуры и осей для графика
         self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.figure)
-
+        self.canvas = FigureCanvas(self.figure)        
         # Создание пользовательской панели инструментов
         self.toolbar = CustomToolbar(self.canvas, self)
-
+        # настройка поведения кастомных действий панели инструментов
+        self.integral_callbacks = IntegralActionCallbacks(self)
+        #self.gauss_callbacks = GaussActionHandler(self)
         # Инициализация переменных для управления затенением и масштабом
         self.shading_regions = []  # Список для хранения затемненных областей
         self.original_xlim = None  # Исходный масштаб оси X
         self.press_x = None        # Координата X при нажатии мыши
         self.press_y = None        # Координата Y при нажатии мыши
         self.mouse_pressed = False # Флаг состояния нажатия мыши
-
         # Подключение обработчиков событий мыши
         self.canvas.mpl_connect('button_press_event', self.on_mouse_press)
         self.canvas.mpl_connect('button_release_event', self.on_mouse_release)
-        self.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)        
-
+        self.canvas.mpl_connect('motion_notify_event', self.on_mouse_move) 
         # Размещение элементов в макете
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         self.setLayout(layout)
-
-    # Обработчик события нажатия кнопки мыши
+    
     def on_mouse_press(self, event):
-        if self.toolbar.integral_action and event.inaxes:
-            self.mouse_pressed = True
-            self.press_x = event.xdata
-            self.original_xlim = self.ax.get_xlim()  # Сохранение текущего масштаба оси X
-            self.ax.set_facecolor('white')
-            self.canvas.draw()
-
-    # Обработчик перемещения мыши с зажатой кнопкой
-    def on_mouse_move(self, event):
-        if self.toolbar.integral_action and self.mouse_pressed and event.xdata and event.inaxes:
-            for region in self.shading_regions:
-                region.remove()
-            self.shading_regions.clear()
-
-            self.ax.set_xlim(self.original_xlim)  # Фиксация масштаба оси X
-
-            # Создание затемненных областей в зависимости от положения мыши
-            if event.xdata < self.press_x:
-                region1 = self.ax.axvspan(self.ax.get_xlim()[0], event.xdata, color='gray', alpha=0.5)
-                region2 = self.ax.axvspan(self.press_x, self.ax.get_xlim()[1], color='gray', alpha=0.5)
+        if self.toolbar.integral_action.isChecked() and event.inaxes:
+            if event.button == 1:   # Левая кнопка мыши
+                self.integral_callbacks.on_press(event)
+            elif event.button == 3: # Правая кнопка мыши
+                self.toolbar.deactivate_all_actions()
             else:
-                region1 = self.ax.axvspan(self.ax.get_xlim()[0], self.press_x, color='gray', alpha=0.5)
-                region2 = self.ax.axvspan(event.xdata, self.ax.get_xlim()[1], color='gray', alpha=0.5)
-
-            self.shading_regions.extend([region1, region2])
-            self.canvas.draw()
-
-    # Обработчик события отпускания кнопки мыши
+                logger.warning("Непредвиденное поведение в on_mouse_press")
+    
+    def on_mouse_move(self, event):
+        if self.toolbar.integral_action.isChecked() and self.mouse_pressed and event.xdata and event.inaxes:
+            self.integral_callbacks.on_move(event)
+    
     def on_mouse_release(self, event):
-        if self.toolbar.integral_action:
-            self.mouse_pressed = False
-            for region in self.shading_regions:
-                region.remove()
-            self.shading_regions.clear()
-            self.ax.set_xlim(self.original_xlim)
-            self.canvas.draw()
-            # Испускание сигнала с координатами
-            if self.press_x is not None and event.xdata is not None:
-                self.mouse_released_signal.emit((self.press_x, event.xdata))
+        if self.toolbar.integral_action.isChecked() and event.inaxes:
+            self.integral_callbacks.on_release(event)
     
     # Слот для отображения данных на графике
     @pyqtSlot(pd.DataFrame, list)
@@ -89,3 +65,4 @@ class GraphicalArea(QWidget):
         # Построение графика по заданным данным
         self.ax.plot(df[column_names[0]], df[column_names[1]],)          
         self.canvas.draw()
+
---
+++ UI/GraphicalArea/integral_action_callbacks.py
@@ -0,0 +1,37 @@
+class IntegralActionCallbacks:
+    def __init__(self, graphical_area):
+        self.graphical_area = graphical_area
+
+    def on_press(self, event):
+        self.graphical_area.mouse_pressed = True
+        self.graphical_area.press_x = event.xdata
+        self.graphical_area.original_xlim = self.graphical_area.ax.get_xlim()
+        self.graphical_area.ax.set_facecolor('white')
+        self.graphical_area.canvas.draw()
+
+    def on_move(self, event):
+        for region in self.graphical_area.shading_regions:
+                region.remove()
+        self.graphical_area.shading_regions.clear()
+        self.graphical_area.ax.set_xlim(self.graphical_area.original_xlim)
+        # Создание затемненных областей в зависимости от положения мыши
+        if event.xdata < self.graphical_area.press_x:
+            region1 = self.graphical_area.ax.axvspan(self.graphical_area.ax.get_xlim()[0], event.xdata, color='gray', alpha=0.5)
+            region2 = self.graphical_area.ax.axvspan(self.graphical_area.press_x, self.graphical_area.ax.get_xlim()[1], color='gray', alpha=0.5)
+        else:
+            region1 = self.graphical_area.ax.axvspan(self.graphical_area.ax.get_xlim()[0], self.graphical_area.press_x, color='gray', alpha=0.5)
+            region2 = self.graphical_area.ax.axvspan(event.xdata, self.graphical_area.ax.get_xlim()[1], color='gray', alpha=0.5)
+        self.graphical_area.shading_regions.extend([region1, region2])
+        self.graphical_area.canvas.draw()
+        
+
+    def on_release(self, event):
+        self.graphical_area.mouse_pressed = False
+        for region in self.graphical_area.shading_regions:
+            region.remove()
+        self.graphical_area.shading_regions.clear()
+        self.graphical_area.ax.set_xlim(self.graphical_area.original_xlim)
+        self.graphical_area.canvas.draw()
+        # Испускание сигнала с координатами
+        if self.graphical_area.press_x is not None and event.xdata is not None:
+            self.graphical_area.mouse_released_signal.emit((self.graphical_area.press_x, event.xdata))
