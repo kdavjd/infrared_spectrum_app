aac3cfcb52c9c047c5c7be81ef14b07bc82706db
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-11-19 12:28:58 +0300

    cumulitive_gauss_func added

--- UI/GraphicalArea/gauss_action_callbacks.py
+++ UI/GraphicalArea/gauss_action_callbacks.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from logger_config import logger
 
 class GaussActionCallbacks:
     def __init__(self, graphical_area):
@@ -14,14 +15,14 @@ class GaussActionCallbacks:
     def on_move(self, event):
         if event.inaxes and self.graphical_area.mouse_pressed:
             w = 2 * abs(self.press_x - event.xdata)
-            x_data = self.graphical_area.ax.lines[0].get_xdata()
-            x = np.linspace(min(x_data), max(x_data), 1000)
+            self.x_data = self.graphical_area.ax.lines[0].get_xdata()
+            x = np.linspace(min(self.x_data), max(self.x_data), 1000)
             y = self.gaussian(x, event.ydata, self.press_x, w)
             if self.gaussian_drawn:
                 self.graphical_area.ax.lines[-1].set_xdata(x)
                 self.graphical_area.ax.lines[-1].set_ydata(y)
             else:
-                self.graphical_area.ax.plot(x, y, 'r--')
+                self.graphical_area.ax.plot(x, y)
                 self.gaussian_drawn = True
             self.graphical_area.canvas.draw()
         
@@ -32,8 +33,20 @@ class GaussActionCallbacks:
                                     'Position': [self.press_x],
                                     'Width': [2 * abs(self.press_x - event.xdata)]})
             self.gaussian_params = pd.concat([self.gaussian_params, new_row], ignore_index=True)
+            self.graphical_area.ax.plot(self.x_data, self.add_cumulitive_gauss_func())
+            self.graphical_area.canvas.draw()
             self.gaussian_drawn = False
+            logger.debug(
+                f"Release event: Height={event.ydata}, Position={self.press_x}, Width={2 * abs(self.press_x - event.xdata)}")
+
+    
+    def add_cumulitive_gauss_func(self) -> np.array:
+        logger.debug(f"Cumulative Gaussian data: {self.gaussian_params}")        
+        return self.gaussian_params.apply(lambda x: self.gaussian(self.x_data, *x), axis=1).sum()
     
     @staticmethod
-    def gaussian(x: np.ndarray, h: float, z: float, w: float) -> np.ndarray:        
+    def gaussian(x: np.ndarray, h: float, z: float, w: float) -> np.ndarray:             
         return h * np.exp(-((x - z) ** 2) / (2 * w ** 2))
+    
+    
+    
