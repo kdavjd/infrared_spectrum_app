0595f6ab4fd8b61057a4a8afe2f094d2c315f3f7
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-11-21 15:44:58 +0300

    display_integral_value moved to integral_action

--- UI/GraphicalArea/integral_action_callbacks.py
+++ UI/GraphicalArea/integral_action_callbacks.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class IntegralActionCallbacks:
     def __init__(self, graphical_area):
         self.graphical_area = graphical_area
@@ -22,8 +24,7 @@ class IntegralActionCallbacks:
             region1 = self.graphical_area.ax.axvspan(self.graphical_area.ax.get_xlim()[0], self.graphical_area.press_x, color='gray', alpha=0.5)
             region2 = self.graphical_area.ax.axvspan(event.xdata, self.graphical_area.ax.get_xlim()[1], color='gray', alpha=0.5)
         self.graphical_area.shading_regions.extend([region1, region2])
-        self.graphical_area.canvas.draw()
-        
+        self.graphical_area.canvas.draw()        
 
     def on_release(self, event):
         self.graphical_area.mouse_pressed = False
@@ -35,3 +36,21 @@ class IntegralActionCallbacks:
         # Испускание сигнала с координатами
         if self.graphical_area.press_x is not None and event.xdata is not None:
             self.graphical_area.mouse_released_signal.emit((self.graphical_area.press_x, event.xdata))
+            self.display_integral_value(event)  
+            
+    def display_integral_value(self, event):        
+        # Определяем начальную и конечную точки для интегрирования
+        start_x = min(self.graphical_area.press_x, event.xdata)
+        end_x = max(self.graphical_area.press_x, event.xdata)            
+        # Выбираем данные для интегрирования
+        mask = (self.graphical_area.x_data >= start_x) & (self.graphical_area.x_data <= end_x)
+        x_selected = self.graphical_area.x_data[mask]
+        y_selected = self.graphical_area.y_data[mask]
+        # Вычисляем интеграл
+        integral_value = np.trapz(y_selected, x_selected)
+        # Отображаем значение интеграла на графике
+        self.graphical_area.ax.text(0.95, 0.05, f'Площадь: {integral_value:.2f}',
+                        verticalalignment='bottom', horizontalalignment='right',
+                        transform=self.graphical_area.ax.transAxes, fontsize=8, bbox=dict(facecolor='white', alpha=0.5))
+        # Перерисовываем холст
+        self.graphical_area.canvas.draw()
