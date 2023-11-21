965d06e27a1a3b5c13aa3091c9a335b6a4c37e15
Avalak <avalak-work@users.noreply.github.com>
2023-11-21 15:36:06 +0400

    Fix assets paths

--- UI/GraphicalArea/custom_toolbar.py
+++ UI/GraphicalArea/custom_toolbar.py
@@ -1,24 +1,27 @@
-from PyQt6.QtGui import QIcon, QAction
-from PyQt6.QtCore import pyqtSignal
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from config import SpectrumConfig
 import pandas as pd
+from matplotlib.backends.backend_qt5agg import \
+    NavigationToolbar2QT as NavigationToolbar
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtGui import QAction, QIcon
+
+from config import SpectrumConfig
+from resources import resource_path
 
 
 class CustomToolbar(NavigationToolbar):
     restore_df_plot_signal = pyqtSignal()
     gauss_action_signal = pyqtSignal()
     integral_action_signal = pyqtSignal(pd.DataFrame)
-    
+
     def __init__(self, canvas, parent):
         super().__init__(canvas, parent)
         self.config = SpectrumConfig()
-        self.create_actions()        
+        self.create_actions()
 
     def create_actions(self):
-        self.integral_action = self.add_action('icons\\integral_icon.png', 'integral')
-        self.restore_df_plot_action = self.add_action('icons\\restore_df_plot_icon.png', 'df_plot', False)
-        self.gauss_action = self.add_action('icons\\gauss_icon.png', 'gauss')
+        self.integral_action = self.add_action(resource_path('icons/integral_icon.png'), 'integral')
+        self.restore_df_plot_action = self.add_action(resource_path('icons/restore_df_plot_icon.png'), 'df_plot', False)
+        self.gauss_action = self.add_action(resource_path('icons/gauss_icon.png'), 'gauss')
 
     def add_action(self, icon_path, action_name, checkable=True):
         action = QAction(QIcon(icon_path), '', self)
