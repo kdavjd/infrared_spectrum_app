b5ba653ab28d640c6e3f417408d4baeffaad59cb
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-11-24 12:04:02 +0300

    Цвета у подписей гауссовых ривых

--- UI/GraphicalArea/gauss_action_callbacks.py
+++ UI/GraphicalArea/gauss_action_callbacks.py
@@ -40,27 +40,37 @@ class GaussActionCallbacks:
             self.gaussian_params.concat_new_gaussian(new_row)
             logger.debug(
                 f"Событие отпуска мыши: высота={event.ydata}, позиция={self.press_x}, ширина={2 * abs(self.press_x - event.xdata)}")
-            
             self.draw_gaussian_curves()
             self.gaussian_drawn = False
 
-    def draw_gaussian_curves(self):
-        # Перерисовываем все кривые на осях
+    def draw_gaussian_curves(self):        
         self.graphical_area.ax.clear()
         self.graphical_area.ax.plot(self.graphical_area.x_data, self.graphical_area.y_data)
-        self.gaussian_params.gaussian_df.apply(
-            lambda row: self.graphical_area.ax.plot(
-                self.x_data, self.gaussian(self.x_data, row['Height'], row['Position'], row['Width'])
-                ), axis=1)          
-        self.graphical_area.ax.plot(self.x_data, self.cumulitive_gauss_func())
-        # Добавляем значение интеграла каждой функции
+        # Список для хранения цветов кривых
+        curve_colors = []
+        # Отрисовка каждой гауссовской кривой и сохранение её цвета
         for index, row in self.gaussian_params.gaussian_df.iterrows():
+            line, = self.graphical_area.ax.plot(
+                self.x_data, self.gaussian(self.x_data, row['Height'], row['Position'], row['Width'])
+            )
+            curve_colors.append(line.get_color())
+        # Отображение кумулятивной кривой
+        self.graphical_area.ax.plot(self.x_data, self.cumulitive_gauss_func())
+        # Добавляем значение интеграла и отображаем квадратик с цветом каждой функции
+        for index, (idx, row) in enumerate(self.gaussian_params.gaussian_df.iterrows()):
             gauss_curve = self.gaussian(self.x_data, row['Height'], row['Position'], row['Width'])
             integral_value = np.trapz(gauss_curve, self.x_data)
-            self.graphical_area.ax.text(0.05, 0.25 - index * 0.03, f"{row['Position']:.2f}, Площадь: {integral_value:.2f}",
-                                        transform=self.graphical_area.ax.transAxes, fontsize=8, verticalalignment='top',
-                                        bbox=dict(facecolor='white', alpha=0.5, edgecolor='none'))
-        self.graphical_area.canvas.draw()            
+            # Получаем цвет линии
+            line_color = curve_colors[index]
+            # Отрисовываем квадратик с цветом
+            self.graphical_area.ax.scatter(
+                [0.045], [0.25 - index * 0.03], color=line_color, transform=self.graphical_area.ax.transAxes, s=100)
+            # Отображаем текст
+            self.graphical_area.ax.text(
+                0.065, 0.25 - index * 0.03, f"{row['Position']:.2f}, Площадь: {integral_value:.2f}",
+                transform=self.graphical_area.ax.transAxes, fontsize=8, verticalalignment='top',
+                bbox=dict(facecolor='white', alpha=0.5, edgecolor='none'))
+        self.graphical_area.canvas.draw()
     
     def cumulitive_gauss_func(self):
         logger.debug(f"Данные gaussian_params в функции кумулятивной линии: {self.gaussian_params.gaussian_df}")        
